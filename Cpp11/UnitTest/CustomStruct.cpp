2550e6d1bdc00e78256e529c4432bf4b3ffa71f5
shaovoon <shaovoon@yahoo.com>
2013-11-21 21:10:24 +0800

    Forgot to check in the CustomStruct.cpp

--- Cpp11/UnitTest/CustomStruct.cpp
+++ Cpp11/UnitTest/CustomStruct.cpp
@@ -3,14 +3,11 @@
 
 Elmax::ostream operator <<(Elmax::ostream& os, const MyStruct& val)
 {
-	std::string temp = boost::lexical_cast<std::string>(val.a);
-	std::wstring str = Elmax::BaseConverter::ConvToString(temp);
-	str += L",";
-	temp = boost::lexical_cast<std::string>(val.b);
-	str += Elmax::BaseConverter::ConvToString(temp);
-	str += L",";
+	os << val.a;
+	os << L",";
+	os << val.b;
+	os << L",";
 
-	os.set_str(os.str() + str);
 	return os;
 }
 
@@ -24,25 +21,23 @@ Elmax::istream operator >>(Elmax::istream& is, MyStruct& val)
 
 Elmax::ostream operator <<(Elmax::ostream& os, const DiffDelimiterStruct& val)
 {
-	std::string temp = boost::lexical_cast<std::string>(val.a);
-	std::wstring str = Elmax::BaseConverter::ConvToString(temp);
-	str += L"|";
-	temp = boost::lexical_cast<std::string>(val.b);
-	str += Elmax::BaseConverter::ConvToString(temp);
-	str += L"|";
+	os << val.a;
+	os << L"|";
+	os << val.b;
+	os << L"|";
 
-	os.set_str(os.str() + str);
 	return os;
 }
 
 Elmax::istream operator >>(Elmax::istream& is, DiffDelimiterStruct& val)
 {
-	Elmax::istream new_is(is.str(), L"|");
+	std::wstring old_delimiter = is.set_delimiter(L"|");
 
-	new_is >> val.a;
-	new_is >> val.b;
+	is >> val.a;
+	is >> val.b;
 
-	is.set_str(new_is.str());
+	is.set_delimiter(old_delimiter);
 
 	return is;
 }
+
